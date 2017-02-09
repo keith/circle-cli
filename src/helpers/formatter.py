492ab87719cdaba902fef071ea72b778070884e7
Keith Smiley <keithbsmiley@gmail.com>
2017-02-08 17:31:14 -0800

    [formatter] Handle unicode better

--- src/helpers/formatter.py
+++ src/helpers/formatter.py
@@ -2,11 +2,11 @@ def output_rows(rows):
     max_lengths = [0] * len(rows[0])
     for row in rows:
         for i, column in enumerate(row):
-            max_lengths[i] = max(len(str(column)), max_lengths[i])
+            max_lengths[i] = max(len(unicode(column)), max_lengths[i])
 
     for row in rows:
         string = ""
         for i, column in enumerate(row):
-            pad = (max_lengths[i] - len(str(column))) * " "
-            string += str(column) + pad + "\t"
+            pad = (max_lengths[i] - len(unicode(column))) * " "
+            string += unicode(column) + pad + "\t"
         print(string)
