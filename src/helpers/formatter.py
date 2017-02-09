0a313b54d70a20a5d5bcb88b92ccdcb7e010f874
Keith Smiley <keithbsmiley@gmail.com>
2017-02-08 19:28:30 -0800

    [formatter] Move printing to caller

--- src/helpers/formatter.py
+++ src/helpers/formatter.py
@@ -1,12 +1,18 @@
 def output_rows(rows):
+    if not rows:
+        return ""
+
     max_lengths = [0] * len(rows[0])
     for row in rows:
         for i, column in enumerate(row):
-            max_lengths[i] = max(len(unicode(column)), max_lengths[i])
+            max_lengths[i] = max(len(str(column)), max_lengths[i])
 
+    output = ""
     for row in rows:
         string = ""
         for i, column in enumerate(row):
-            pad = (max_lengths[i] - len(unicode(column))) * " "
-            string += unicode(column) + pad + "\t"
-        print(string)
+            pad = (max_lengths[i] - len(str(column))) * " "
+            string += str(column) + pad + "\t"
+        output += string.strip() + "\n"
+
+    return output
