f3191450a32acc6083156aebc9151297ad41fc72
Keith Smiley <keithbsmiley@gmail.com>
2018-07-17 21:06:05 -0700

    Add command to get single build

--- src/helpers/circle.py
+++ src/helpers/circle.py
@@ -3,6 +3,7 @@ from helpers import credentials
 import requests
 import sys
 
+
 URL_TEMPLATE = "https://circleci.com/api/v1.1/project/{}/{}/{}"
 
 
@@ -19,7 +20,24 @@ def get_builds(provider, repo, count, branch, all_builds, only_running=False):
         params["filter"] = "running"
 
     response = requests.get(url, params=params)
-    if response.status_code != 200:
+    if response.status_code == 404:
+        print("Got 404 from Circle", file=sys.stderr)
+        sys.exit(1)
+    elif response.status_code != 200:
+        print(response.json(), file=sys.stderr)
+        sys.exit(1)
+
+    return response.json()
+
+
+def get_build(provider, repo, build_num):
+    url = URL_TEMPLATE.format(provider, repo, build_num)
+    password = credentials.credentials()
+    response = requests.get(url, params={"circle-token": password})
+    if response.status_code == 404:
+        print("Got 404 from Circle", file=sys.stderr)
+        sys.exit(1)
+    elif response.status_code != 200:
         print(response.json(), file=sys.stderr)
         sys.exit(1)
 
